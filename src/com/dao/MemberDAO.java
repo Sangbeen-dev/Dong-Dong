19be13f53866274921bcd061b305a59ba62e1eb3
knoah <ssh7636@gmail.com>
2021-04-14 16:28:47 +0900

    login

--- src/com/dao/MemberDAO.java
+++ src/com/dao/MemberDAO.java
@@ -1,16 +1,21 @@
 package com.dao;
 
+import java.util.HashMap;
+
 import org.apache.ibatis.session.SqlSession;
 
 import com.dto.MemberDTO;
 
 public class MemberDAO {
-	
-	
+
 	public MemberDTO mypage(SqlSession session, String userid) {
 		MemberDTO dto = session.selectOne("MemberMapper.mypage", userid);
 		return dto;
-	}//end myPage
-	
-}//end MemberDAO
+	}// end myPage
 
+	public MemberDTO login(SqlSession session, HashMap<String, String> map) {
+		MemberDTO dto = session.selectOne("MemberMapper.login", map);
+		return dto;
+	} //end login
+
+}// end MemberDAO
