58df9b014ad568b0f7c0996f516d69cc6c1f019e
sangbeen-dev <tkdqls960@naver.com>
2021-04-14 18:15:16 +0900

    수정

--- src/com/controller/member/FavoriteServlet.java
+++ src/com/controller/member/FavoriteServlet.java
@@ -1,11 +1,15 @@
 package com.controller.member;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.dto.MemberDTO;
 
 /**
  * Servlet implementation class FavoriteServlet
@@ -13,7 +17,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/FavoriteServlet")
 public class FavoriteServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		HttpSession session = request.getSession();
+		MemberDTO dto = (MemberDTO) session.getAttribute("login");
+		String nextPage = null;
+		if(dto!=null) {
+			
+		}
 	}
 
 	/**
