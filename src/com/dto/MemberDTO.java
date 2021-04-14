e13f26cac9088a57fa421e1a4f87dd1ecd4bb3bc
sangbeen-dev <tkdqls960@naver.com>
2021-04-14 15:02:10 +0900

    기본파일

---
+++ src/com/dto/MemberDTO.java
@@ -0,0 +1,84 @@
+package com.dto;
+
+public class MemberDTO {
+	private String userid;
+	private String passwd;
+	private String username;
+	private String nickName;
+	private String addr;
+	private String phone;
+	private String email1;
+	private String email2;
+	public MemberDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public MemberDTO(String userid, String passwd, String username, String nickName, String addr, String phone,
+			String email1, String email2) {
+		super();
+		this.userid = userid;
+		this.passwd = passwd;
+		this.username = username;
+		this.nickName = nickName;
+		this.addr = addr;
+		this.phone = phone;
+		this.email1 = email1;
+		this.email2 = email2;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail1() {
+		return email1;
+	}
+	public void setEmail1(String email1) {
+		this.email1 = email1;
+	}
+	public String getEmail2() {
+		return email2;
+	}
+	public void setEmail2(String email2) {
+		this.email2 = email2;
+	}
+	@Override
+	public String toString() {
+		return "MemberDTO [userid=" + userid + ", passwd=" + passwd + ", username=" + username + ", nickName="
+				+ nickName + ", addr=" + addr + ", phone=" + phone + ", email1=" + email1 + ", email2=" + email2 + "]";
+	}
+
+	
+	
+}
