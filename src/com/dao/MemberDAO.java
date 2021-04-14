218b348f87da731c3acfd6ebe2a804f7fd3eb4f6
sangbeen-dev <tkdqls960@naver.com>
2021-04-14 17:00:07 +0900

    수정

--- src/com/dao/MemberDAO.java
+++ src/com/dao/MemberDAO.java
@@ -11,15 +11,12 @@ public class MemberDAO {
 	public MemberDTO mypage(SqlSession session, String userid) {
 		MemberDTO dto = session.selectOne("MemberMapper.mypage", userid);
 		return dto;
-<<<<<<< feature/noah
 	}// end myPage
 
 	public MemberDTO login(SqlSession session, HashMap<String, String> map) {
 		MemberDTO dto = session.selectOne("MemberMapper.login", map);
 		return dto;
 	} //end login
-=======
-	}//end myPage
 
 	public int memberAdd(SqlSession session,MemberDTO dto) {
 		   int n = session.insert("MemberMapper.memberAdd", dto);
@@ -27,6 +24,4 @@ public class MemberDAO {
 	   }
 	
 }//end MemberDAO
->>>>>>> master
 
-}// end MemberDAO
