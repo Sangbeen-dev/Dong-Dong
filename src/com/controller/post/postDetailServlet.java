231dae02be914490a1da38a360adda817c0c325f
git2kd <sk10dlek@naver.com>
2021-04-14 19:44:35 +0900

    post detail page servlet/service/dao update

--- src/com/controller/post/postDetailServlet.java
+++ src/com/controller/post/postDetailServlet.java
@@ -1,6 +1,7 @@
 package com.controller.post;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -16,12 +17,14 @@ public class postDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	String pNum = request.getParameter("pNum");
+    	//String pNum = request.getParameter("pNum");
+    	String pNum = "5"; // 테스트 값
     	
     	PostService service = new PostService();
     	
-    	//HashMap<String, String> map = service.getPostDetailByPNum(Integer.parseInt(pNum));
+    	HashMap<String, String> map = service.getPostDetailByPNum(Integer.parseInt(pNum));
     	
+    	System.out.println(map);
     	// 데이터 파싱 후 페이지 이동
     	
     	RequestDispatcher dis = request.getRequestDispatcher("postDetailPage.jsp");
