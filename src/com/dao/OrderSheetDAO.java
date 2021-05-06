e790e21d52dd01de90a07074a3f2f65d2f803a09
sangbeen-dev <tkdqls960@naver.com>
2021-05-06 17:06:53 +0900

    주문요청함(구매확정빼고)

--- src/com/dao/OrderSheetDAO.java
+++ src/com/dao/OrderSheetDAO.java
@@ -1,7 +1,10 @@
 package com.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
+import com.dto.MyOrderSheetDTO;
 import com.dto.OrderSheetDTO;
 
 public class OrderSheetDAO {
@@ -11,6 +14,26 @@ public class OrderSheetDAO {
 		int num = session.insert("OrderSheetAdd", dto);
 		
 		return num;
+	}
+
+	public List<MyOrderSheetDTO> ordersheetList(SqlSession session, String userid) {
+		List<MyOrderSheetDTO> list = session.selectList("OrderSheetMapper.ordersheetList", userid);
+		return list;
+	}
+
+	public int orderDel(SqlSession session, int num) {
+		int n = session.delete("OrderSheetMapper.orderDel", num);
+		return n;
+	}
+
+	public int orderAllDel(SqlSession session, List<String> list) {
+		int n = session.delete("OrderSheetMapper.orderAllDel", list);
+		return n;
+	}
+
+	public List<MyOrderSheetDTO> message(SqlSession session, String oNum) {
+		List<MyOrderSheetDTO> list = session.selectList("OrderSheetMapper.message", oNum);
+		return list;
 	}
 
 }
